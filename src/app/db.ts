0aeeecb64c128e3fc77deecca6e507c8e254d76d
tannv6 <tannv6@gmail.com>
2024-01-22 21:31:00 +0700

    admin

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,16 +1,15 @@
-import config from '@/server/config';
 import mysql from 'mysql2/promise';
 
 const connectDB = async () => {
   try {
     const connection = await mysql.createConnection({
-      host: config.DATABASE_HOST,
+      host: 'localhost',
       user: 'brillebe_root',
       password: 'B.@vZWMly_qW',
       database: 'brillebe_root',
     });
 
-    console.log('Connected to the database', process.env.DATABASE_HOST);
+    console.log('Connected to the database');
     return connection;
   } catch (error) {
     console.error('Error connecting to the database:', error);
