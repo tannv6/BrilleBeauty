a377a38724b1a052e8d5db55c330d234b1414f64
tannv6 <tannv6@gmail.com>
2024-01-23 12:51:33 +0700

    admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -21,19 +21,15 @@ function Main({response} : any) {
   }
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-[url('/backgroud_admin.png')]">
         <Link
           href=""
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          <img className="w-8 h-8 mr-2" src="" alt="logo" />
           BrilleBeauty
         </Link>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-[360px] max-w-full bg-white rounded-lg shadow dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your manager system {response}
-            </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={onSubmit}>
               <div>
                 <label
@@ -75,7 +71,6 @@ function Main({response} : any) {
                       aria-describedby="remember"
                       type="checkbox"
                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                      required={true}
                     />
                   </div>
                   <div className="ml-3 text-sm">
