b30dfde405e870c09c340c9129dda36b56f00f71
XuanTruong <hdnk.it@gmail.com>
2024-01-24 01:44:18 +0700

    product_detail
    done

--- src/components/ProductDetailNav.tsx
+++ src/components/ProductDetailNav.tsx
@@ -5,8 +5,8 @@ export default function ProductDetailNav({ tab }: any) {
         <>
             <div className="flex justify-center">
                 <Link href="#product_in4" className={`flex items-center border justify-center w-[300px] h-14 ${(tab==1) ? "bg-[#ef426f] text-[#ffffff] border-0" : ""}`}>Product information</Link>
-                <a href="" className={`flex items-center border border-l-0 justify-center w-[300px] h-14 ${(tab==2) ? "bg-[#ef426f] text-[#ffffff] border-0" : ""}`}>Product review(36)</a>
-                <a href="" className={`flex items-center border border-l-0 justify-center w-[300px] h-14 ${(tab==3) ? "bg-[#ef426f] text-[#ffffff] border-0" : ""}`}>Related product</a>
+                <Link href="#product_rvw" className={`flex items-center border border-l-0 justify-center w-[300px] h-14 ${(tab==2) ? "bg-[#ef426f] text-[#ffffff] border-0" : ""}`}>Product review(36)</Link>
+                <Link href="#product_rlt" className={`flex items-center border border-l-0 justify-center w-[300px] h-14 ${(tab==3) ? "bg-[#ef426f] text-[#ffffff] border-0" : ""}`}>Related product</Link>
             </div>
         </>
     );
