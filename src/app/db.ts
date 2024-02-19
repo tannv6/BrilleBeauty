402b8a9d66c6563e057ff44fed2dd2e2bb51349e
tannv6 <tannv6@gmail.com>
2024-02-19 10:11:08 +0700

    db info

--- src/app/db.ts
+++ src/app/db.ts
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 const connectDB = async () => {
   try {
     const connection = await mysql.createConnection({
-      host: '14.225.254.140',
-      user: 'brillebeauty',
-      password: 'brillebeauty@@24',
-      database: 'brillebeauty',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
     });
     console.log('Connected to the database');
     return connection;
