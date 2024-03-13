d70e7911ff16ed5a3572e106bed1ea18b95245a4
tannv6 <tannv6@gmail.com>
2024-03-13 08:13:21 +0700

    pop

--- src/components/MainPopup.tsx
+++ src/components/MainPopup.tsx
@@ -32,7 +32,7 @@ function MainPopup({ isOpen, onClose, title, children, popup }: Props) {
                 aria-hidden="true"
                 className={`${
                   e.IsShow ? "" : "hidden"
-                } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+                } ${e.IsCentered ? "flex" : ""} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
               >
                 <div className="relative p-4 w-full max-w-xl max-h-full">
                   {/* Modal content */}
@@ -40,7 +40,7 @@ function MainPopup({ isOpen, onClose, title, children, popup }: Props) {
                     {/* Modal header */}
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                       <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                        {title}
+                        {e.PopupTitle}
                       </h3>
                       <button
                         onClick={() => {
@@ -69,14 +69,15 @@ function MainPopup({ isOpen, onClose, title, children, popup }: Props) {
                       </button>
                     </div>
                     {/* Modal body */}
-                    <div className="p-4 md:p-5 space-y-4">
+                    <div className="space-y-4">
+                      <div className="w-full pt-[100%] relative">
                       <Image
-                        className="mt-2"
+                        className="object-cover"
                         src={`${CDN_URL}${e.PopupImage}`}
                         alt={""}
-                        width={100}
-                        height={100}
+                        fill
                       />
+                      </div>
                     </div>
                     {/* Modal footer */}
                     <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
