3a8a84eeef972b6172fff5407efa84d933959c30
tannv6 <tannv6@gmail.com>
2024-03-13 08:32:26 +0700

    pop

--- src/pages/api/popups/detail.ts
+++ src/pages/api/popups/detail.ts
@@ -17,9 +17,6 @@ export default async function handle(
 
     if (Array.isArray(result) && result.length > 0) {
       const popup: any = result[0];
-      popup["IsShow"] = popup["IsShow"][0];
-      popup["IsFull"] = popup["IsFull"][0];
-      popup["IsCentered"] = popup["IsCentered"][0];
       return res.status(200).json(popup);
     } else {
       return res.status(200).json(null);
