bcfdf3bb61e964b3c0f66583ffde066e8271107c
dat <datvbhp123@gmail.com>
2024-03-29 17:03:43 +0700

    test

--- src/pages/api/cart/list.ts
+++ src/pages/api/cart/list.ts
@@ -11,7 +11,7 @@ export default async function handle(
     const query = `select c1.*, p1.ProductName, p1.ProductImage, p2.PoName, p2.PoSellPrice from cart as c1 left join products as p1 on c1.ProductID = p1.ProductID
             left join product_options as p2 on c1.PoID = p2.PoID where c1.CustomerID = '${customerID}'and  p2.DeletedAt is null`;
    
-    const [result] = await connect.execute(query);
+    const [result] = await connect.execute(query)
 
     connect.end();
     if (Array.isArray(result) && result.length > 0) {
